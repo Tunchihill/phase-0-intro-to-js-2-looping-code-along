ffb481f6d45f506cecba13a840d66e58c3831aa8
Arnold Kiprono Langat <arnoldkiprono3@gmail.com>
2024-04-03 14:03:01 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards( stringName, events ) {
+    let thankYou = []
+    for ( let i = 0; i < stringName.length; i++ ) {
+      thankYou.push( `Thank you, ${stringName[i]}, for the wonderful ${events} gift!` )
+    }
+    return thankYou
+  }
+  
+  function countDown( num1 ) {
+    while ( num1 > 0 ) {
+      console.log( num1 );
+      num1 -= 1;
+    }
+    console.log( num1 );
+  }
